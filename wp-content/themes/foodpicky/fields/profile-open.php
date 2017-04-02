b85c8de57c243d4d0a84b673ad68f2d9af130891
Yong Qui Zheng <zheng4uga@gmail.com>
2017-04-02 19:04:58 -0400

    fixed language translation and fixed profile open issue

--- wp-content/themes/foodpicky/fields/profile-open.php
+++ wp-content/themes/foodpicky/fields/profile-open.php
@@ -7,8 +7,18 @@
 <?php
 
 global $post;
+
 date_default_timezone_set('America/New_York');
-$hours = get_post_meta($post->ID, 'working-hours-' . date('N') . '-hours');
+$postId;
+if($post->post_type =="product"){
+       $author_id = $post->post_author;
+       $get_vendor_filter = array('post_type'=>'azl_profile','author__in'=>array($author_id));
+       $vendor = get_posts($get_vendor_filter);
+       $postId =$vendor->ID;
+}else{
+    $postId = $post->ID;
+}
+$hours = get_post_meta($postId, 'working-hours-' . date('N') . '-hours');
 if(in_array(date('G'), $hours)) {
     print '<div class="is-open open-now">'.  esc_html__('Open', 'foodpicky').'</div>';
 } else {
