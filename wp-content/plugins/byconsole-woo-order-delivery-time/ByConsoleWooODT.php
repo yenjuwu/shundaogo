782b207b82805801f878c0af13a2ddebf47d4fc9
Yong Qui Zheng <zheng4uga@gmail.com>
2017-04-03 08:54:56 -0400

    date

--- wp-content/plugins/byconsole-woo-order-delivery-time/ByConsoleWooODT.php
+++ wp-content/plugins/byconsole-woo-order-delivery-time/ByConsoleWooODT.php
@@ -352,11 +352,12 @@ function byconsolewooodt_checkout_field( $checkout ) {
 								),
 
         ), $checkout->get_value( 'byconsolewooodt_delivery_type' ));
-        /*
+        
 	woocommerce_form_field( 'byconsolewooodt_delivery_date', array(
 
         'type'          => 'text',
         'class'         => array('byconsolewooodt_delivery_date'),
+           'attributes' =>array('disabled'=>'disabled'),
 
         'label'         => get_option('byconsolewooodt_chekout_page_date_lebel'),
 
@@ -365,8 +366,7 @@ function byconsolewooodt_checkout_field( $checkout ) {
 		'default'		=> $byconsolewooodt_delivery_widget_cookie_array['byconsolewooodt_widget_date_field'],
 
         ), $checkout->get_value( 'byconsolewooodt_delivery_date' ));
-         * 
-         */
+   
 
 	woocommerce_form_field( 'byconsolewooodt_delivery_time', array(
 
